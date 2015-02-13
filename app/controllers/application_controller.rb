5fc04c1979c2e492ec8942d2e902f5f43847d73b
Arjuna Shankar <arjuna.shankar@gmail.com>
2015-02-13 18:06:01 +0530

    fixed devise and user registration form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :extras
+  # before_action :configure_devise_permitted_parameters, if: :devise_controller?
+
+  protected
+
+    def extras
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name,:nationality, :middle_name, :gender, :title, :institutional_affiliation, :phone, :address, :pio, :oci, :guardian_names, :date_of_birth, :place_of_birth, :passport_number, :passport_place, :passport_date_of_issue, :passport_place_of_issue, :passport_date_of_expiry, :address_as_stated_in_your_passport, :indian_consulate, :role ,:email, :password, :password_confirmation) }
+    end
 end
