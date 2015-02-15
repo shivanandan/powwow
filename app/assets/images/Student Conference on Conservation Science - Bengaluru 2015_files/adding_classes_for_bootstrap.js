10503388a5e69f57000473798b5f35c386186526
Arjuna Shankar <arjuna.shankar@gmail.com>
2015-02-15 15:44:51 +0530

    fixed register

--- app/assets/images/Student Conference on Conservation Science - Bengaluru 2015_files/adding_classes_for_bootstrap.js
+++
@@ -1,7 +0,0 @@
-function add_classes() {
-	$('#error_explanation').addClass('alert alert-dismissible alert-warning');
-	$('#error_explanation').prepend('<button type="button" class="close" data-dismiss="alert">×</button>')
-}
-
-$(document).ready(add_classes);
-$(document).on('page:load', add_classes);
