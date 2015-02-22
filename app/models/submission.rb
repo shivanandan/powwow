c037649d38a4162d1387958cd972d36ae5a69332
Arjuna Shankar <arjuna.shankar@gmail.com>
2015-02-22 12:30:56 +0530

    added model validations

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,7 +4,11 @@ class Submission < ActiveRecord::Base
 	has_attached_file :visual_abstract, :styles => { :large => "1200x1200", :medium => "800x800>", :thumb => "300x300>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :visual_abstract, :content_type => /\Aimage\/.*\Z/
 
-	validates_presence_of :introduction, :methods, :results, :discussion, :title, :authors, :co_authors, :gps, :poster, :previous_attendance, :attendance_year, :previous_bursary, :bursary, :reason_for_bursary
+	validates_presence_of :introduction, :methods, :results, :discussion, :title, :authors, :gps_lat, :gps_lng, :poster, :previous_attendance, :previous_bursary, :bursary
 
 	validates_length_of :title, :maximum => 20, :too_long => " must be under 20 words.", :tokenizer => lambda { |str| str.split }
+	validates_length_of :introduction, :maximum => 100, :too_long => " must be under 20 words.", :tokenizer => lambda { |str| str.split }
+	validates_length_of :methods, :maximum => 125, :too_long => " must be under 20 words.", :tokenizer => lambda { |str| str.split }
+	validates_length_of :results, :maximum => 125, :too_long => " must be under 20 words.", :tokenizer => lambda { |str| str.split }
+	validates_length_of :discussion, :maximum => 20, :too_long => " must be under 20 words.", :tokenizer => lambda { |str| str.split }
 end
