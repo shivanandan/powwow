1bd03023a82b915ba7422099f17bf32db710feca
SEEMA SHENOY <arjuna.shankar@gmail.com>
2015-03-12 05:29:37 +0530

    allowed false for previous bursary and current bursary

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,7 +4,7 @@ class Submission < ActiveRecord::Base
 	has_attached_file :visual_abstract, :styles => { :large => "1200x1200", :medium => "800x800>", :thumb => "300x300>" }, :default_url => "/images/:style/missing.png"
 	validates_attachment_content_type :visual_abstract, :content_type => /\Aimage\/.*\Z/
 
-	validates_presence_of :introduction, :methods, :results, :discussion, :title, :gps_lat, :gps_lng, :previous_attendance, :bursary, :presenter, :current_institution, :keywords
+	validates_presence_of :introduction, :methods, :results, :discussion, :title, :gps_lat, :gps_lng, :presenter, :current_institution, :keywords
 
 	validates_length_of :title, :maximum => 20, :too_long => " must be under 20 words.", :tokenizer => lambda { |str| str.split }
 	validates_length_of :introduction, :maximum => 100, :too_long => " must be under 20 words.", :tokenizer => lambda { |str| str.split }
