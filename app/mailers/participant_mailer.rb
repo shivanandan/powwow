4c67eec391ade9ae3399d96b61ceca2a17ecc3ad
Arjun Shankar <arjuna.shankar@gmail.com>
2015-05-21 08:50:25 +0530

    added mailer

--- app/mailers/participant_mailer.rb
+++ app/mailers/participant_mailer.rb
@@ -19,4 +19,12 @@ class ParticipantMailer < ActionMailer::Base
     @submission = submission
     mail(to: @user.email, subject: subject)
   end
+
+  def inform_user_of_status(user, submission, reviews)
+    @user = user
+    @submission = submission
+    @reviews = reviews
+    # mail(to: @user.email, subject: 'SCCS-Bengaluru 2015 : New Abstracts Assigned')
+    mail(to: 'arjuna.shankar@gmail.com', subject: 'SCCS-Bengaluru 2015 : New Abstracts Assigned')
+  end
 end
